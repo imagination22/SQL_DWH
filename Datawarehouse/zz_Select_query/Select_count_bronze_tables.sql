8098da17f441b4ac4a3f363aded8818ca5eda339
Shubham Barman <barmanshubham1822@gmail.com>
2025-06-10 20:59:06 +0530

    initialize ,DDL , SP , select bronze layer

---
+++ Datawarehouse/zz_Select_query/Select_count_bronze_tables.sql
@@ -0,0 +1,8 @@
+--execute bronze.load_bronze
+
+Select count(*) from bronze.crm_cust_info
+Select count(*) from bronze.crm_prd_info
+Select count(*) from bronze.crm_sales_details
+Select count(*) from bronze.erp_cust_az12
+Select count(*) from bronze.erp_loc_a101
+Select count(*) from bronze.erp_px_cat_g1v2
